4f644d2dd457ac7c873a2f43251eeabdf0b93d3d
James Tlaloc <jamestlaloc.dev@gmail.com>
2026-02-12 17:40:19 -0800

    Change let to const.

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 let orders = [{table: 1, item: "Burger", priority: false}, {table: 2, item: "Fries", priority: true}, {table: 3, item: "Coke", priority: false}];
 
 function processOrders(orders) {
-  let formattedOrders = [];
+  const formattedOrders = [];
   
 
   for(let i = 0; i < orders.length; i++){
-    let order = orders[i];
+    const order = orders[i];
     let formattedString = `Table ${order.table}: ${order.item}`;
 
     if(order.priority) {
