ce039917d0474480e9d342b8ebeafa17996c269d
James Tlaloc <jamestlaloc.dev@gmail.com>
2026-02-12 23:23:10 -0800

    Refactor code using map function instead of loop and push.

--- script.js
+++ script.js
@@ -1,22 +1,15 @@
 let orders = [{table: 1, item: "Burger", priority: false}, {table: 2, item: "Fries", priority: true}, {table: 3, item: "Coke", priority: false}];
 
 function processOrders(orderList) {
-  const processedOrders = [];
-
-  for(let i = 0; i < orderList.length; i++){
-    const order = orders[i];
-
+  return orderList.map(function (order){
     const status = order.priority ? "priority" : "normal";
 
-    const processedOrder = {
+    return processedOrder = {
       table: order.table,
       item: order.item,
       status
     };
-
-    processedOrders.push(processedOrder);
   }
-  return processedOrders;
 }
 
 let result = processOrders(orders);
