618b335dcd309dce7653181fb3cc796df5b4b936
James Tlaloc <jamestlaloc.dev@gmail.com>
2026-02-12 17:35:48 -0800

    Rerite object-based version of code from scratch.

--- script.js
+++ script.js
@@ -1,20 +1,40 @@
-let orders = ["Burger", "Fries", "Coke"]
+let orders = [{table: 1, item: "Burger", priority: false}, {table: 2, item: "Fries", priority: true}, {table: 3, item: "Coke", priority: false}];
 
 function processOrders(orders) {
   let formattedOrders = [];
+  
 
   for(let i = 0; i < orders.length; i++){
-    let formattedString = `Order ${i + 1}: ${orders[i]}`;
+    let order = orders[i];
+    let formattedString = `Table ${order.table}: ${order.item}`;
 
-    if(orders[i] === "Coke") {
-      formattedString = `Order ${i + 1}: ${orders[i]} (Out of Stock)`;
-      
-    };
+    if(order.priority) {
+      formattedString = `PRIORITY!!! - Table ${order.table}: ${order.item}`;
+    }
     formattedOrders.push(formattedString);
-  };
-
+  }
   return formattedOrders;
 }
 
-const result = processOrders(orders);
+let result = processOrders(orders);
+
 console.log(result);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
