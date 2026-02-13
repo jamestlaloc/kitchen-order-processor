ce8e37a661346ddbf79aa6432b0cc3f2a253c068
James Tlaloc <jamestlaloc.dev@gmail.com>
2026-02-12 18:18:26 -0800

    Rewrite so it returns an array of objects without mutating the original order.

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
 let orders = [{table: 1, item: "Burger", priority: false}, {table: 2, item: "Fries", priority: true}, {table: 3, item: "Coke", priority: false}];
 
 function processOrders(orders) {
-  const formattedOrders = [];
-  
+  const processedOrders = [];
 
   for(let i = 0; i < orders.length; i++){
     const order = orders[i];
-    let formattedString = `Table ${order.table}: ${order.item}`;
 
-    if(order.priority) {
-      formattedString = `PRIORITY!!! - Table ${order.table}: ${order.item}`;
-    }
-    formattedOrders.push(formattedString);
+    const status = order.priority ? "priority" : "normal";
+
+    const processedOrder = {
+      table: order.table,
+      item: order.item,
+      status: status
+    };
+
+    processedOrders.push(processedOrder);
   }
-  return formattedOrders;
+  return processedOrders;
 }
 
 let result = processOrders(orders);
